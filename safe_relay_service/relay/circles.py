0890ec1ebbbdcae5494f95bf1aa2b03ea73a8cf5
llunaCreixent <lluna@bitspossessed.org>
2022-11-03 01:52:47 +0100

    Remove pyethereum dependency

--- safe_relay_service/relay/circles.py
+++ safe_relay_service/relay/circles.py
@@ -13,7 +13,7 @@ class Circles:
         return self.gas_price
 
     def pack_address(self, address):
-        assert check_checksum(address)
+        assert Web3.isChecksumAddress(address)
         return "000000000000000000000000" + address[2:]
 
     def is_circles_token(self, address):
