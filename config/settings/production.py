2969befee0192235c2613690f8c0f72c778036dd
JacqueGM <jacquemate@riseup.net>
2023-04-20 16:02:27 +0200

    update corst regex

--- config/settings/production.py
+++ config/settings/production.py
@@ -58,7 +58,6 @@ INSTALLED_APPS += ["gunicorn"]
 
 # Django CORS
 
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 CORS_ALLOWED_ORIGIN_REGEXES = env.list("CORS_ALLOWED_ORIGIN_REGEXES", default=[r"^https:\/\/\w+\.circles\.garden$"])
 
 # SAFE
